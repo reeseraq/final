82e885081b7c017591b481619baa06091e9c32c7
reeseraq <reeserob@uchicago.edu>
2024-05-14 21:17:30 -0500

    a bunch of new things including timer

--- javapart.js
+++ javapart.js
@@ -1,7 +1,14 @@
 
 
-let sec = 0
+let s = 0
 
 let counter = document.querySelector('#counter')
 
-counter.textContent = 'work in progress'
+counter.textContent = 'let the time begin...'
+
+function count() {
+  s = s + 1
+  counter.textContent = s
+}
+
+setInterval(count, 500)
